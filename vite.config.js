ad98e2c0d31b9ae91342fc3fa9cb07e1600dd199
AyoubLegouirah <ayoubgrand@outlook.fr>
2023-11-17 10:04:15 +0100

    mise a jour

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import copy from "rollup-plugin-copy";
+
+export default defineConfig({
+  base: "/meteo/", // Remplacez '/chemin-de-votre-application/' par la base souhaitée
+
+  build: {
+    rollupOptions: {
+      output: {
+        dir: "dist",
+      },
+    },
+  },
+
+  plugins: [
+
+    copy({
+      targets: [ 
+        { src: "images", dest: "dist" }, // Copier le répertoire "images" dans "dist/images"
+      ],
+      hook: "writeBundle",
+    }),
+  ],
+});
